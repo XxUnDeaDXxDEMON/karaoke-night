6393527f0e296887954a7518d517d13641958d56
JessicaAKelley <kelleyajessica@gmail.com>
2021-06-17 17:23:56 -0500

    Saved poster images working

--- script.js
+++ script.js
@@ -17,7 +17,9 @@ var apiFunction = function () {
       var i = Math.floor(Math.random() * 10);
       Img.setAttribute("src", response.Search[i].Poster);
       Img.setAttribute("height", 400);
+      Img.setAttribute("id", "imagePoster");
       container.appendChild(Img);
+    
 
       // ---------------------------------------------------------------------data 2: movie title
       var movieTitle = document.querySelector("#movieTitle");
@@ -71,11 +73,12 @@ var apiFunction = function () {
 //--------------------------------------------RUN ON WEBSITE LOAD-----------------------------------
 var saveList = localStorage.getItem('poster');
 if(saveList == null){
-  var saveList = [] //Create as empty array if no local storage
+  saveList = [] //Create as empty array if no local storage
   //alert("local storage is empty");
 }
 else{
-  var saveList = [localStorage.getItem('poster')]; //Create local storage as an array
+  saveList = JSON.parse(localStorage.getItem('poster')); //Create local storage as an array
+  
   //alert("local storage has something");
 }
 alert(saveList);
@@ -83,31 +86,81 @@ alert(saveList);
 document.getElementById("searchBtn").onclick = apiFunction;
 document.getElementById("saveBtn").onclick = posterSave;
 
-//--------------------------------------------RUN ON SAVE-----------------------------------
+//--------------------------------------------RUN ON SAVE----------------------------------
 function posterSave(){
-  saveList.push(movieId.innerHTML);
+  var imgNode = document.getElementById("imagePoster");
+  var imgSource = imgNode.getAttribute("src");
+    console.log(imgSource);
+  var obj = {
+    id: movieId.innerHTML,
+    img: imgSource
+  }
+  saveList.push(obj);
   //alert(saveList.length); //Debug to check the length of the local storage array
   if(saveList.length > 5) saveList.shift();
   alert(saveList[0]);
 
-  localStorage.setItem('poster', saveList);
+  localStorage.setItem('poster', JSON.stringify (saveList));
+
+  var containerTwo = document.querySelector("#hearted-movie"); //Figure out where to load this
+      //containerTwo.innerHTML = "";
+      var ImgTwo = document.createElement("img");
+      //var iTwo = Math.floor(Math.random() * response.Search.length);
+      ImgTwo.setAttribute("src", imgSource);
+      ImgTwo.setAttribute("height", 400);
+      containerTwo.appendChild(ImgTwo);
 
   //Save movie poster when save button is clicked
   // // API to fetch http://www.omdbapi.com/?i=tt0120338
+  return
   fetch(`http://www.omdbapi.com/?i=${saveList[0]}&apikey=46b2d125`)
     .then(function (response) {
       return response.json();
     })
     .then(function (response) {
       console.log(response);
-    });
+
       var containerTwo = document.querySelector("#hearted-movie"); //Figure out where to load this
       containerTwo.innerHTML = "";
       var ImgTwo = document.createElement("img");
-      var iTwo = Math.floor(Math.random() * 10);
+      var iTwo = Math.floor(Math.random() * response.Search.length);
       ImgTwo.setAttribute("src", response.Search[iTwo].Poster);
       ImgTwo.setAttribute("height", 400);
-      containerTwo.appendChild(Img);
+      containerTwo.appendChild(ImgTwo);
   //End posted display
+    });
 
 }
+
+function displayPoster(){
+  var saveList = localStorage.getItem('poster');
+  if(saveList == null){
+    saveList = [] //Create as empty array if no local storage
+    //alert("local storage is empty");
+  }
+  else{
+    saveList = JSON.parse(localStorage.getItem('poster')); //Create local storage as an array
+    
+    //alert("local storage has something");
+  }
+  var hearted = document.getElementById("hearted-movie")
+  
+  
+
+  for (let i = 0; i < saveList.length; i++) {
+    
+    var savedItem = saveList[i]
+    var elem = document.createElement("img");
+    elem.setAttribute("src", savedItem.img)
+    elem.setAttribute("alt", savedItem.id)
+
+    hearted.appendChild(elem);
+
+    // hearted.innerHTML += `
+    //   <img src="${saveList[i].img}" alt="${saveList[i].id}"/>
+    // `
+
+  }
+};
+
+displayPoster()
