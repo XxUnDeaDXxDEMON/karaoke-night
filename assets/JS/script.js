610898752a4eae1b0915942c6a2bc86a2e91b836
Jane Lu <jyl.janelu@gmail.com>
2021-06-17 17:29:22 -0700

    test

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -15,7 +15,7 @@ var apiFunction = function () {
       var Img = document.createElement("img");
       var i = Math.floor(Math.random() * 10);
       Img.setAttribute("src", response.Search[i].Poster);
-      Img.setAttribute("height", 400);
+      Img.setAttribute("height", 600);
       Img.setAttribute("id", "imagePoster");
       container.appendChild(Img);
       // ---------------------------------------------------------------------data 2: movie title
@@ -59,13 +59,12 @@ var apiFunction = function () {
     });
 };
 //--------------------------------------------RUN ON WEBSITE LOAD-----------------------------------
-var saveList = localStorage.getItem('poster');
-if(saveList == null){
-  saveList = [] //Create as empty array if no local storage
+var saveList = localStorage.getItem("poster");
+if (saveList == null) {
+  saveList = []; //Create as empty array if no local storage
   //alert("local storage is empty");
-}
-else{
-  saveList = JSON.parse(localStorage.getItem('poster')); //Create local storage as an array
+} else {
+  saveList = JSON.parse(localStorage.getItem("poster")); //Create local storage as an array
   //alert("local storage has something");
 }
 //alert(saveList);
@@ -73,29 +72,29 @@ else{
 document.getElementById("searchBtn").onclick = apiFunction;
 document.getElementById("saveBtn").onclick = posterSave;
 //--------------------------------------------RUN ON SAVE----------------------------------
-function posterSave(){
+function posterSave() {
   var imgNode = document.getElementById("imagePoster");
   var imgSource = imgNode.getAttribute("src");
-    console.log(imgSource);
+  console.log(imgSource);
   var obj = {
     id: movieId.innerHTML,
-    img: imgSource
-  }
+    img: imgSource,
+  };
   saveList.push(obj);
   //alert(saveList.length); //Debug to check the length of the local storage array
-  if(saveList.length > 5) saveList.shift();
+  if (saveList.length > 5) saveList.shift();
   //alert(saveList[0]);
-  localStorage.setItem('poster', JSON.stringify (saveList));
+  localStorage.setItem("poster", JSON.stringify(saveList));
   var containerTwo = document.querySelector("#hearted-movie"); //Figure out where to load this
-      //containerTwo.innerHTML = "";
-      var ImgTwo = document.createElement("img");
-      //var iTwo = Math.floor(Math.random() * response.Search.length);
-      ImgTwo.setAttribute("src", imgSource);
-      ImgTwo.setAttribute("height", 400);
-      containerTwo.appendChild(ImgTwo);
+  //containerTwo.innerHTML = "";
+  var ImgTwo = document.createElement("img");
+  //var iTwo = Math.floor(Math.random() * response.Search.length);
+  ImgTwo.setAttribute("src", imgSource);
+  ImgTwo.setAttribute("height", 200);
+  containerTwo.appendChild(ImgTwo);
   //Save movie poster when save button is clicked
   // // API to fetch http://www.omdbapi.com/?i=tt0120338
-  return
+  return;
   fetch(`http://www.omdbapi.com/?i=${saveList[0]}&apikey=46b2d125`)
     .then(function (response) {
       return response.json();
@@ -109,29 +108,29 @@ function posterSave(){
       ImgTwo.setAttribute("src", response.Search[iTwo].Poster);
       ImgTwo.setAttribute("height", 400);
       containerTwo.appendChild(ImgTwo);
-  //End posted display
+      //End posted display
     });
 }
-function displayPoster(){
-  var saveList = localStorage.getItem('poster');
-  if(saveList == null){
-    saveList = [] //Create as empty array if no local storage
+function displayPoster() {
+  var saveList = localStorage.getItem("poster");
+  if (saveList == null) {
+    saveList = []; //Create as empty array if no local storage
     //alert("local storage is empty");
-  }
-  else{
-    saveList = JSON.parse(localStorage.getItem('poster')); //Create local storage as an array
+  } else {
+    saveList = JSON.parse(localStorage.getItem("poster")); //Create local storage as an array
     //alert("local storage has something");
   }
-  var hearted = document.getElementById("hearted-movie")
+  var hearted = document.getElementById("hearted-movie");
   for (let i = 0; i < saveList.length; i++) {
-    var savedItem = saveList[i]
+    var savedItem = saveList[i];
     var elem = document.createElement("img");
-    elem.setAttribute("src", savedItem.img)
-    elem.setAttribute("alt", savedItem.id)
+    elem.setAttribute("src", savedItem.img);
+    elem.setAttribute("alt", savedItem.id);
     hearted.appendChild(elem);
+    hearted.classList = "poster-history";
     // hearted.innerHTML += `
     //   <img src="${saveList[i].img}" alt="${saveList[i].id}"/>
     // `
   }
-};
-displayPoster()
+}
+displayPoster();
