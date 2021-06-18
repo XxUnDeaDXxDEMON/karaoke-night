04d18491b1a5ec7999dced14e9636a9876fd18a6
Richard <rasteavens@yahoo.com>
2021-06-17 17:58:20 -0700

    develop

---
+++ movie-night-main/README.md
@@ -0,0 +1,61 @@
+# Group 4 - Movie Night
+
+## Group Members:
+
+Chao Deng, Jessica Kelley, Jane Lu, and Richard Steavens
+
+## Preview:
+
+Check out [Movie Night] (link)!
+
+[![website demo] (gif)](link)
+
+## Concept:
+
+Our goal was to create a space for users to search movies and receive pertinent information about the specific movie they searched. Further, we wanted to enable users to favorite a select list of up to five movies that they can go back to next time they visit our website. By using two movie APIs to fetch information including title, poster, year, ID, and synopsis, Movie Night is a one stop shop for all things movie information.
+
+## Process:
+
+Technologies Used:
+
+- HTML
+- CSS
+- JavaScript
+- APIs
+- JSON
+
+Tasks & Roles:
+
+- Overall Idea: The Group
+- GitHub Repository Creation: Richard
+- HTML & CSS: Jane
+- JavaScript APIs: Chao
+- JavaScript Local Storage: Jessica
+- README: Jessica
+- Presentation Slides: Jessica
+- Presentation: The Group
+
+Challenges:
+
+- Conceptualizing and streamlining project idea and keeping it within scope of skill set
+- Finding API source that worked
+  - Data limitation
+  - Cost
+  - Copyright
+- Determining how to divide up the work
+- Time difference amongst group members (PST & CST)
+- Getting user saved movie IDs to display
+
+Successes:
+
+- Working with a team for the first time
+- Working through merge conflicts
+- Debugging to get code to work
+- Completing the project
+
+## Future Direction
+
+- Work through bugs in the API itself
+- Create amd implement a section to display if the selected movie is available to stream and on which specific streaming services it is available (if any)
+- Enable user to create a profile/account
+- Implement drag/drop function to allow user to place their saved movies in a specific order ranking (1-5)
---
+++ movie-night-main/assets/JS/script.js
@@ -0,0 +1,137 @@
+// ---------------------------------------------FIRST API: MOVIE -------------------------------------
+// Create a function called `myFunction()`
+var apiFunction = function () {
+  var search = document.querySelector("#searchTerm").value;
+  // Make a `fetch` request from MOVIE
+  fetch(`http://www.omdbapi.com/?s=${search}&apikey=46b2d125`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+      // ---------------------------------------------------------------------data 1: movie poster
+      var container = document.querySelector("#response-container");
+      container.innerHTML = "";
+      var Img = document.createElement("img");
+      var i = Math.floor(Math.random() * 10);
+      Img.setAttribute("src", response.Search[i].Poster);
+      Img.setAttribute("height", 400);
+      Img.setAttribute("id", "imagePoster");
+      container.appendChild(Img);
+      // ---------------------------------------------------------------------data 2: movie title
+      var movieTitle = document.querySelector("#movieTitle");
+      movieTitle.innerHTML = response.Search[i].Title;
+      // ---------------------------------------------------------------------data 3: movie year
+      var movieYear = document.querySelector("#movieYear");
+      movieYear.innerHTML = response.Search[i].Year;
+      // ---------------------------------------------------------------------data 4: movie imdbID
+      var movieId = document.querySelector("#movieId");
+      movieId.innerHTML = response.Search[i].imdbID;
+      //--------------------------------------------SECOND API: WIKIPEDIA------------------------------------
+      // get the title from Movie response, and link it to wiki search
+      var title = response.Search[i].Title;
+      console.log("MOVIE TITLE:" + title);
+      // Make a `fetch` request from WIKIPEDIA
+      return fetch(
+        `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&exsentences=8&titles=${title}&origin=*`
+      );
+    })
+    .then(function (wikiResponse) {
+      return wikiResponse.json();
+    })
+    .then(function (wikiResponse) {
+      console.log(wikiResponse);
+      // ---------------------------------------------------------------------data 5: wikipedia description
+      // this is tricky, get the pageID in wikipedia using for function
+      var pageData = wikiResponse.query.pages;
+      var pageKey;
+      for (let key in pageData) {
+        console.log(key, pageData[key]);
+        pageKey = key;
+      }
+      console.log(wikiResponse.query.pages[pageKey].extract);
+      // display the wiki description
+      var wikiContainer = document.querySelector("#wikiResponse");
+      wikiContainer.innerHTML = "";
+      var wikiText = document.createElement("P");
+      wikiText.innerHTML = wikiResponse.query.pages[pageKey].extract;
+      wikiContainer.append(wikiText);
+    });
+};
+//--------------------------------------------RUN ON WEBSITE LOAD-----------------------------------
+var saveList = localStorage.getItem('poster');
+if(saveList == null){
+  saveList = [] //Create as empty array if no local storage
+  //alert("local storage is empty");
+}
+else{
+  saveList = JSON.parse(localStorage.getItem('poster')); //Create local storage as an array
+  //alert("local storage has something");
+}
+//alert(saveList);
+//--------------------------------------------RUN THE API FUNCTION-----------------------------------
+document.getElementById("searchBtn").onclick = apiFunction;
+document.getElementById("saveBtn").onclick = posterSave;
+//--------------------------------------------RUN ON SAVE----------------------------------
+function posterSave(){
+  var imgNode = document.getElementById("imagePoster");
+  var imgSource = imgNode.getAttribute("src");
+    console.log(imgSource);
+  var obj = {
+    id: movieId.innerHTML,
+    img: imgSource
+  }
+  saveList.push(obj);
+  //alert(saveList.length); //Debug to check the length of the local storage array
+  if(saveList.length > 5) saveList.shift();
+  //alert(saveList[0]);
+  localStorage.setItem('poster', JSON.stringify (saveList));
+  var containerTwo = document.querySelector("#hearted-movie"); //Figure out where to load this
+      //containerTwo.innerHTML = "";
+      var ImgTwo = document.createElement("img");
+      //var iTwo = Math.floor(Math.random() * response.Search.length);
+      ImgTwo.setAttribute("src", imgSource);
+      ImgTwo.setAttribute("height", 400);
+      containerTwo.appendChild(ImgTwo);
+  //Save movie poster when save button is clicked
+  // // API to fetch http://www.omdbapi.com/?i=tt0120338
+  return
+  fetch(`http://www.omdbapi.com/?i=${saveList[0]}&apikey=46b2d125`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+      var containerTwo = document.querySelector("#hearted-movie"); //Figure out where to load this
+      containerTwo.innerHTML = "";
+      var ImgTwo = document.createElement("img");
+      var iTwo = Math.floor(Math.random() * response.Search.length);
+      ImgTwo.setAttribute("src", response.Search[iTwo].Poster);
+      ImgTwo.setAttribute("height", 400);
+      containerTwo.appendChild(ImgTwo);
+  //End posted display
+    });
+}
+function displayPoster(){
+  var saveList = localStorage.getItem('poster');
+  if(saveList == null){
+    saveList = [] //Create as empty array if no local storage
+    //alert("local storage is empty");
+  }
+  else{
+    saveList = JSON.parse(localStorage.getItem('poster')); //Create local storage as an array
+    //alert("local storage has something");
+  }
+  var hearted = document.getElementById("hearted-movie")
+  for (let i = 0; i < saveList.length; i++) {
+    var savedItem = saveList[i]
+    var elem = document.createElement("img");
+    elem.setAttribute("src", savedItem.img)
+    elem.setAttribute("alt", savedItem.id)
+    hearted.appendChild(elem);
+    // hearted.innerHTML += `
+    //   <img src="${saveList[i].img}" alt="${saveList[i].id}"/>
+    // `
+  }
+};
+displayPoster()
